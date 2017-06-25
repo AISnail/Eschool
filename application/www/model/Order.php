c3ac20faa6850d1ddcdcafd8ab4eccbcf027d1a8
zpc <svip_zpc@163.com>
2017-06-25 16:58:22 +0800

    user personal center & user login

---
+++ application/www/model/Order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/25
+ * Time: 15:29
+ */
+
+namespace app\www\model;
+
+use think\Model;
+
+class Order extends Model
+{
+    protected $table = 'eschool_order';
+}
--- application/www/model/User.php
+++ application/www/model/User.php
@@ -2,9 +2,16 @@
 
 namespace app\www\model;
 
+use think\Db;
 use think\Model;
 
 class User extends Model
 {
 	protected $table = 'eschool_user';
+
+	public function getUserInfo($username = '')
+	{
+		//$info = Db::name('eschool_user')->where('username','eq',$username)->whereOr('loginname','eq',$username)->find();
+		//return $info;
+	}
 }
